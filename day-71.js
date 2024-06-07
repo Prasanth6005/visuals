cef2dc24d6c47a3a62bf268384284d37edc63355
Prasanth6005 <prasanthmooki@gmail.com>
2024-06-07 09:45:34 -0700

    Add files via upload
    It is sample project of background colors are changed by the given options and using the technologies like HTML, CSS & JS

---
+++ day-71.js
@@ -0,0 +1,36 @@
+function color() {
+
+    if(document.getElementById("prasanth").value=="red") {
+        document.getElementById("mooki").style="background-color:red;"
+    }
+
+   else if(document.getElementById("prasanth").value=="blue") {
+        document.getElementById("mooki").style="background-color:blue;"
+    }
+
+    if(document.getElementById("prasanth").value=="green") {
+        document.getElementById("mooki").style="background-color:green;"
+    }
+
+    if(document.getElementById("prasanth").value=="yellow") {
+        document.getElementById("mooki").style="background-color:yellow;"
+    }
+
+    if(document.getElementById("prasanth").value=="orange") {
+        document.getElementById("mooki").style="background-color:orange;"
+    }
+
+    if(document.getElementById("prasanth").value=="brown") {
+        document.getElementById("mooki").style="background-color:brown;"
+    }
+
+    if(document.getElementById("prasanth").value=="grey") {
+        document.getElementById("mooki").style="background-color:grey;"
+    }
+
+    if(document.getElementById("prasanth").value=="pink") {
+        document.getElementById("mooki").style="background-color:pink;"
+    }
+
+}
+
